33b0dfec5f5c55414b129eca21679b03957fea90
dkfagh <dkfagh@gmail.com>
2020-11-07 11:10:20 +0000

    #2.11

--- indeed.py
+++ indeed.py
@@ -6,12 +6,13 @@ URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
 
 
 def get_last_page():
+    # URL에 request를 요청
     result = requests.get(URL)
 
-    # indeed_result에서 html코드를 추출
+    # result에서 html코드를 추출
     soup = BeautifulSoup(result.text, "html.parser")
 
-    # indeed_soup에서 clss가 pagination인 div를 검색
+    # soup에서 clss가 pagination인 div를 검색
     pagination = soup.find("div", {"class": "pagination"})
 
     # pagination에서 anchor태그를 전부 검색
@@ -31,13 +32,16 @@ def get_last_page():
 
 
 def extract_job(html):
-    # 입력받은 html코드 중 h2태그의 class가 title인 코드에서 a태그가 title인 코드를 추출
+    # 입력받은 html코드에서 class가 title인 h2를 검색하고 그 결과에서 a태그를 검색하여 어트리뷰트 title을 추출
     title = html.find("h2", {"class": "title"}).find("a")["title"]
-    # 추출한 title에서 span태그의 class가 company인 결과를 추출
+
+    # 입력받은 html코드에서 class가 company인 span 결과를 검색
     company = html.find("span", {"class": "company"})
+
     if company:
-      # 추출한 company에서 a태그를 추출
+      # 검색된 company에서 a태그를 검색
       company_anchor = company.find("a")
+
       # company_anchor가 존재하면
       if company_anchor is not None:
           company = str(company_anchor.string)
@@ -49,17 +53,21 @@ def extract_job(html):
     else:
       company = None
 
+    # 입력받은 html코드에서 class가 recJobLoc인 div를 검색하고 어트리뷰트 data-rc-loc을 추출
     location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]
+    # 입력받은 html코드에서 어트리뷰트 data-jk를 추출
     job_id = html["data-jk"]
+
     # title과 company를 배열로 return
     return {
-        'title': title,
-        'company': company,
-        'location': location,
+        "title": title,
+        "company": company,
+        "location": location,
         "link": f"https://www.indeed.com/viewjob?jk={job_id}"
     }
 
 
+# 변수로 입력받은 last_page의 크기만큼 반복 (0 ~ last_page)
 def extract_jobs(last_page):
 
     # 빈 배열 jobs 생성
@@ -68,11 +76,12 @@ def extract_jobs(last_page):
     # 반복문으로 동적인 url값을 생성하여 result값에 저장 (last_page의 값 만큼 반복)
     for page in range(last_page):
         print(f"scrapping page {page}")
+        # URL&start={page*LIMIT}에 request를 요청
         result = requests.get(f"{URL}&start={page*LIMIT}")
-        # indeed_result에서 html코드를 추출
+        # result에서 html코드를 추출
         soup = BeautifulSoup(result.text, "html.parser")
 
-        # 일자리 목록을 추출
+        # 추출한 soup에서 class가 jobsearch-SerpJobCard인 div를 전부 검색
         results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
 
         # 반복문을 통해 extract_job의 return값을 jobs 배열에 저장
@@ -80,7 +89,6 @@ def extract_jobs(last_page):
             job = extract_job(result)
             jobs.append(job)
     return jobs
-
 
 
 def get_jobs():
--- so.py
+++ so.py
@@ -8,9 +8,21 @@ def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, "html.parser")
     pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
-    last_page = pages[-2].get_text(strip=True)
+    last_page = pages[-2].get_text(strip = True)
 
     return int(last_page)
+
+
+def extract_job(html):
+  title = html.find("h2", {"class":"mb4"}).find("a")["title"]
+
+  # 검색된 항목이 2개 이상일 경우 변수를 따로 선언
+  # recursive=False : 하위레벨의 검색을 막아줌
+  company, location = html.find("h3", {"class":"mb4"}).find_all("span", recursive = False)
+
+  print(company.get_text(strip = True), location.get_text(strip = True))
+  
+  return {"title":title}
 
 
 def extract_jobs(last_page):
@@ -22,7 +34,10 @@ def extract_jobs(last_page):
         results = soup.find_all("div", {"class": "-job"})
 
         for result in results:
-            print(result["data-jobid"])
+            job = extract_job(result)
+            jobs.append(job)
+
+    return jobs
 
 
 def get_jobs():
